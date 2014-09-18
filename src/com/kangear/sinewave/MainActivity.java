0b2405c58fd86ee13cb5a5a2eddbc3142f7c7a69
kangear <kangear@163.com>
2014-09-18 11:11:43 +0800

    add MainAcitivity content

--- src/com/kangear/sinewave/MainActivity.java
+++ src/com/kangear/sinewave/MainActivity.java
@@ -1,22 +1,80 @@
 package com.kangear.sinewave;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    // originally from http://marblemice.blogspot.com/2010/04/generate-and-play-tone-in-android.html
+    // and modified by Steve Pomeroy <steve@staticfree.info>
+    private final int duration = 10; // seconds
+    private final int sampleRate = 44100;
+    private final int numSamples = duration * sampleRate;
+    private final double sample[] = new double[numSamples];
+    private final double freqOfTone = 200000; // hz  200000=>20khz(50us) 最高
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    private final byte generatedSnd[] = new byte[2 * numSamples];
+    
+    WaveService mWaveService = new WaveService();
+
+    Handler handler = new Handler();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Use a new tread as this can take a while
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                genTone();
+                handler.post(new Runnable() {
+
+                    public void run() {
+                        playSound();
+                    }
+                });
+            }
+        });
+        thread.start();
+    }
+
+    void genTone(){
+        // fill out the array
+        for (int i = 0; i < numSamples; ++i) {
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+        }
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (final double dVal : sample) {
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+        }
+    }
+
+    void playSound(){
+    	int length = mWaveService.getWave((short)0x707, (byte)0x05).length;
+        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, length,
+                AudioTrack.MODE_STATIC);
+        audioTrack.write(mWaveService.getWave((short)0x707, (byte)0x05), 0, length);
+        audioTrack.play();
+    }
 
 }
